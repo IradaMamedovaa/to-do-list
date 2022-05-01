6ae57d41162f42686382bbe21e317b315c91fc86
Irada Mamedova <iradamamedovaa17@gmail.com>
2022-05-01 23:37:37 +0400

    better version

--- app.js
+++ app.js
@@ -4,37 +4,31 @@ const input = document.querySelector('input');
 const clearBtn = document.querySelector('.clearBtn');
 
 let tasks = [];
-let checked = [];
 
 window.addEventListener('load', (e) => {
     let storedTasks = JSON.parse(localStorage.getItem('tasks'));
-    let checkedTasks = JSON.parse(localStorage.getItem('checked'));
     
     if(storedTasks){
         tasks = [...storedTasks];
         storedTasks.forEach(task => {
-            list.innerHTML+= `<li><i class="fas fa-check-circle check"></i><span>${task}</span><i class="far fa-trash-alt"></i></li> `;
-        })
-    }
-
-    let listItems =Array.from(list.children);
-    if(checkedTasks){
-        checked = [...checkedTasks];
-        listItems.forEach(li => {
-            if(checked.includes(li.innerText)) {
-                li.classList.add('done');
+            list.innerHTML+= `<li><i class="fas fa-check-circle check"></i><span>${task.title}</span><i class="far fa-trash-alt"></i></li> `;
+            let listItems = list.querySelectorAll('li');
+            if(task.checked == true){
+                listItems.forEach(li => {
+                    if(task.title == li.innerText) li.classList.add('done');
+                })
             }
         })
-    }
     trash();
     check();
+    }
 });
 
 addBtn.addEventListener('click', (e) => {
     e.preventDefault();
     if(!(input.value.trim())) return;
     list.innerHTML+= `<li><i class="fas fa-check-circle check"></i><span>${input.value}</span><i class="far fa-trash-alt"></i></li> `;
-    tasks.push(input.value);
+    tasks.push({title: input.value, checked : false});
     localStorage.setItem('tasks',JSON.stringify(tasks));
     input.value = "";
     trash();
@@ -44,9 +38,7 @@ addBtn.addEventListener('click', (e) => {
 clearBtn.addEventListener('click', (e) => {
     list.innerHTML = '';
     tasks = [];
-    checked = [];
     localStorage.setItem('tasks',JSON.stringify(tasks));
-    localStorage.setItem('checked',JSON.stringify(checked));
 })
 
 function trash(){
@@ -54,10 +46,8 @@ function trash(){
     trashBtns.forEach((btn) => {
         btn.addEventListener('click',(e)=>{
             e.target.parentElement.remove();
-            tasks = tasks.filter(task => task != e.target.parentElement.innerText);
-            checked = checked.filter(task => task != e.target.parentElement.innerText);
+            tasks = tasks.filter(task => task.title != e.target.previousElementSibling.innerText);
             localStorage.setItem('tasks',JSON.stringify(tasks));
-            localStorage.setItem('checked',JSON.stringify(checked));
         })
     })
 }
@@ -68,15 +58,10 @@ function check(){
     checkInputs.forEach((i) => { 
         i.addEventListener('click',(e) => {
             e.target.parentElement.classList.toggle("done");
-            if(e.target.parentElement.classList.contains('done')){
-                checked.push(e.target.parentElement.innerText);
-                console.log(e.target.parentElement);
-                localStorage.setItem('checked',JSON.stringify(checked));
-            }else{
-                checked = checked.filter(task => task != e.target.parentElement.innerText);
-                localStorage.setItem('checked',JSON.stringify(checked));
-            }
+            tasks.forEach(task => {
+                if(task.title == e.target.nextElementSibling.innerText) task.checked = !task.checked;
+                localStorage.setItem('tasks',JSON.stringify(tasks));        
+            })
         })
     })
 }
-
